f70364be02cf31ab651da1843a2b7f8ac1521aa1
pratik-gangurde-128 <pratikgangurde128@gmail.com>
2026-03-02 16:04:04 +0530

    Day 2 Updated

--- Day-2/server.js
+++ Day-2/server.js
@@ -2,8 +2,24 @@ const express = require("express")
 
 const app = express() 
 
+app.use(express.json())
+
+const notes = []
+
 app.get('/',(req,res) => {
     res.send("Home Page")
 })
 
-app.listen(3000)
+app.post('/notes',(req,res) => {
+    console.log(req.body)
+    notes.push(req.body)
+    res.send("Notes Has been Created")
+})
+
+app.get('/notes',(req,res) => {
+    res.send(notes)
+})
+
+app.listen(3000 , () =>{
+    console.log("Server is Running on Port 3000")
+})
