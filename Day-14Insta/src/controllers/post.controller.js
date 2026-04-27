7f7819f41df32c98329b8d3c8b6f71f89e0d0399
pratik-gangurde-128 <pratikgangurde128@gmail.com>
2026-04-27 12:05:25 +0530

    Day-14 Posts Details API's

---
+++ Day-14Insta/src/controllers/post.controller.js
@@ -0,0 +1,149 @@
+const postModel = require("../models/post.model")
+const ImageKit = require("@imagekit/nodejs")
+const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken") // Token Generate keliye
+
+
+const imagekit = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+})
+
+
+async function createPostController(req,res){
+    console.log(req.body,req.file) // req.body mein data aata hai   req.file mein file aati hai(eg: image)
+
+    
+    const token = req.cookies.token // Token ki request kar rahe hai
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Token not provided, Unauthorized access"
+        })
+    }
+
+
+    // Verify kar rahe hai ki token humare server ne hi create kiya ya nahi
+    // Decoded ke andar same data aata hai jo 
+    // token create karte waqt hum pass kar rahe they
+
+    let decoded = null //Scoping ki wajah decoded null diya hai
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "user not authorized"
+        })
+    }
+
+
+    console.log(decoded)
+
+    const file = await imagekit.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), 'file'),
+    fileName: "Test",
+    folder: "cohort-2-insta-posts" //Specific Folder mein upload karne keliye
+    })
+
+    // res.send(file)
+
+    const post = await postModel.create({
+    caption: req.body.caption,
+    imgUrl: file.url,
+    user: decoded.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully.",
+        post
+    })
+
+}
+
+
+async function getPostController(req, res) {
+
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "UnAuthorized Access"
+        })
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "Token invalid"
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200)
+        .json({
+            message: "Posts fetched successfully.",
+            posts
+        })
+
+}
+
+
+async function getPostDetailsController(req, res) {
+
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "UnAuthorized Access"
+        })
+    }
+
+    let decoded
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid Token"
+        })
+    }
+    const userId = decoded.id // Jo bhi user ne request kee hogi uski id mil jayegi
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId) //Post id ke basis par post find karenge
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId   // Check ho raha hai ki Post ka USER(owner) kon hai , 
+                                                            // and compare kar raha hai konse user ne request ki hai
+                                                            //Kyuki Real User(Owner Of the Post) hi Khudki Createkiye hue post dekh sakta hai
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content."
+        })
+    }
+
+    return res.status(200).json({
+        message: "Post fetched  successfully.",
+        post
+    })
+
+}
+
+
+module.exports = {
+    createPostController,
+    getPostController,
+    getPostDetailsController
+    }   
---
+++ Day-14Insta/src/routes/post.routes.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const postRouter = express.Router()
+const postController = require("../controllers/post.controller")
+const multer = require("multer") // Express ko capable banata hai (File) Read karna jo frontend se aata hai
+const upload = multer({ storage: multer.memoryStorage() }) // File store kar rahe hai
+
+/**
+ * POST /api/posts [protected]
+ * - req.body = { caption,image-file }
+ */
+postRouter.post("/",upload.single("image"),postController.createPostController) // upload.single("image") ==> ("image") mein same name aana chahiye
+                                                                                // jo frontend se bheja ho
+
+
+/**
+ * GET /api/posts/ [protected]
+ */
+postRouter.get("/", postController.getPostController)
+
+
+/**
+ * GET /api/posts/details/:postid
+ * - return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
+ */
+postRouter.get("/details/:postId", postController.getPostDetailsController)
+
+module.exports = postRouter
