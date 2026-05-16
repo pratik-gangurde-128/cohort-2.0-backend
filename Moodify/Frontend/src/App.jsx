fb122dcebba00c5e42fd34d6c4ef4d759000c8f3
pratik-gangurde-128 <pratikgangurde128@gmail.com>
2026-05-16 12:40:27 +0530

    Auth and Autharisation Completed

--- Moodify/Frontend/src/App.jsx
+++ Moodify/Frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import FaceExpression from './features/Expression/components/FaceExpression'
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes"
+import "./features/shared/styles/global.scss"
+import { AuthProvider } from "./features/auth/auth.context"
+// import { SongContextProvider } from "./features/home/song.context"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <FaceExpression/>
+    <AuthProvider>
+      {/* <SongContextProvider> */}
+        <RouterProvider router={router} />
+      {/* </SongContextProvider> */}
+    </AuthProvider>
   )
 }
 
